83525367bb33bc21c7b3efc99f05bc34e96e3387
luciano mendez <mendezluciano23@gmail.com>
2023-09-26 21:30:32 -0300

    actua

--- src/components/Scene/pc.jsx
+++ src/components/Scene/pc.jsx
@@ -20,7 +20,7 @@ class PC extends Component {
       loadingComplete: false,
       executables: [
         { id: 1, name: 'Calisthenics', url: 'https://lucianomendez1.github.io/Calisthenics./', isOpen: false, icon: icoCalistenics },
-        { id: 2, name: 'Psychedelic2.0', url: 'https://psychedelic2-0.vercel.app/', isOpen: false, icon: browserIcon },
+        { id: 2, name: 'Psychedelic 2.0', url: 'https://psychedelic2-0.vercel.app/', isOpen: false, icon: browserIcon },
         { id: 3, name: 'Art Palm', url: 'https://ecommerce-art-git-master-lucianomendez1.vercel.app/', isOpen: false, icon: textEditorIcon },
         { id: 4, name: 'Psychedelic1.0', url: 'https://lucianomendez1.github.io/Psychodelic/', isOpen: false, icon: psychelic },
         { id: 5, name: 'Galaxy', url: 'https://galaxy-wheat.vercel.app/', isOpen: false, icon: galaxy },
